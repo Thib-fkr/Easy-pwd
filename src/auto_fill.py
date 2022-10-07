880fb818725a335b6c9b04a25037d9f7f22087b0
Thib-fkr <grignard.thibault@gmail.com>
2022-10-07 12:02:33 +0200

    Began to implement auto_fill feature with selenium webdriver.

---
+++ src/auto_fill.py
@@ -0,0 +1,44 @@
+
+from yaml import load, Loader
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
+from manager import Manager as Man
+
+def main():
+    
+    man = Man()
+    
+    # Firefox has to be launched with --marionette arg
+    driver = webdriver.Firefox(service=Service(executable_path="selenium-driver/geckodriver", service_args=["--marionette-port", "2828", "--connect-existing"]))
+    
+    # Get active url
+    url = driver.current_url
+    
+    # Retrieve websites list
+    with open("data/sites/0.yml") as f:
+        enc_sites : list[dict[int, dict]] = load(f, Loader=Loader)["website"]
+
+    # Look through each website name to find if one corresponds to the active url
+    for enc_site in enc_sites[1:]:
+        
+        # Store associated file number and isolate useful data
+        for number in enc_site.keys():
+            enc_site_2 = enc_site[number]
+        
+        clear_site = man.decrypt(hash=enc_site_2["hash"], salt=enc_site_2["salt"])
+        
+        # Load and decrypt authentication information from correct file
+        if clear_site.decode() == url:
+            with open(f"data/sites/{number}.yml") as f:
+                enc_auth : dict = load(f, Loader=Loader)
+            clear_auth = \
+                (man.decrypt(hash=enc_auth["username"], salt=enc_auth["salt1"]).decode() , \
+                man.decrypt(hash=enc_auth["password"], salt=enc_auth["salt2"]).decode())
+            print(clear_auth)
+            
+            
+
+if __name__ == "__main__":
+    main()
